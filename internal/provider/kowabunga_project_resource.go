1848bfef05f1513bba6a31bd86ac3730df81ba8e
Benjamin Zores <benjamin.zores@gmail.com>
2024-03-28 09:56:45 +0100

    project: use missing map pplan modifier

--- internal/provider/kowabunga_project_resource.go
+++ internal/provider/kowabunga_project_resource.go
@@ -14,6 +14,7 @@ import (
 	"github.com/hashicorp/terraform-plugin-framework/resource"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema/int64default"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema/mapplanmodifier"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema/planmodifier"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema/stringdefault"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema/stringplanmodifier"
@@ -158,6 +159,9 @@ func (r *ProjectResource) Schema(ctx context.Context, req resource.SchemaRequest
 				Computed:            true,
 				MarkdownDescription: "List of project's private subnets zones association (read-only)",
 				ElementType:         types.StringType,
+				PlanModifiers: []planmodifier.Map{
+					mapplanmodifier.UseStateForUnknown(),
+				},
 			},
 			KeyGroups: schema.ListAttribute{
 				MarkdownDescription: "The list of user groups allowed to administrate the project (i.e. capable of managing internal resources)",
