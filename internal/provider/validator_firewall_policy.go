233aecc5b2eb0bf8c2c1fc200ea86ff4f537a849
Benjamin Zores <benjamin.zores@gmail.com>
2024-05-29 12:10:46 +0200

    add firewall policy validator

---
+++ internal/provider/validator_firewall_policy.go
@@ -0,0 +1,45 @@
+package provider
+
+import (
+	"context"
+	"fmt"
+	"slices"
+
+	"github.com/hashicorp/terraform-plugin-framework/schema/validator"
+)
+
+const (
+	ValidatorFirewallPolicyDescription    = "Protocol must be one of 'accept', 'drop'"
+	ValidatorFirewallPolicyErrUnsupported = "Unsupported policy"
+)
+
+var firewallSupportedPolicy = []string{
+	"accept",
+	"drop",
+}
+
+type stringFirewallPolicyValidator struct{}
+
+func (v stringFirewallPolicyValidator) Description(ctx context.Context) string {
+	return ValidatorFirewallPolicyDescription
+}
+
+func (v stringFirewallPolicyValidator) MarkdownDescription(ctx context.Context) string {
+	return v.Description(ctx)
+}
+
+func (v stringFirewallPolicyValidator) ValidateString(ctx context.Context, req validator.StringRequest, resp *validator.StringResponse) {
+
+	if req.ConfigValue.IsUnknown() || req.ConfigValue.IsNull() {
+		return
+	}
+
+	protocol := req.ConfigValue.ValueString()
+	if !slices.Contains(firewallSupportedPolicy, protocol) {
+		resp.Diagnostics.AddAttributeError(
+			req.Path,
+			ValidatorFirewallPolicyErrUnsupported,
+			fmt.Sprintf("%s: %s", ValidatorFirewallPolicyErrUnsupported, protocol),
+		)
+	}
+}
